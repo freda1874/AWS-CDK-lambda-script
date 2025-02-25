4e0345d2be85e2c0551788c2ce28a772cdba5c6c
fredaluo <freda.luo@gmail.com>
2025-02-25 10:24:58 -0500

    add caching

--- lambda/main.py
+++ lambda/main.py
@@ -1,8 +1,10 @@
-import os 
-def handler(event, context): 
-    version = os.environ.get("VERSION","1.0.0")
-    response_body ={
-        "message":"Hello World!",
-        "Version":"1.0.0"
+import os
+
+
+def handler(event, context):
+    version = os.environ.get("VERSION", "1.0.0")
+    response_body = {
+        "message": "Hello World 🚀",
+        "Version": "1.0.0"
     }
     return {"statusCode": 200, "body": response_body}
